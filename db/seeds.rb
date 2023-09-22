edbb762f4bc0d915e6e1f84bd5071efd1735ec56
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2023-09-22 16:39:04 -0500

    Part 4: Add fav movies to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,15 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
    :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-   :release_date => '13-Aug-2021'}
+   :release_date => '13-Aug-2021'},
+  {:title => 'Inception', :rating => 'PG-13',
+   :release_date => '16-Jul-2010'},
+  {:title => 'Interstellar', :rating => 'PG-13',
+   :release_date => '07-Nov-2014'},
+  {:title => 'Shutter Island', :rating => 'R',
+   :release_date => '19-Feb-2010'},
+  {:title => 'Schindler\'s List', :rating => 'R',
+   :release_date => '15-Dec-1993'}
 ]
 
 more_movies.each do |movie|
